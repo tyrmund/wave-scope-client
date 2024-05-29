01b9ac56bc552bd5f4a8f50fb3cb3a7e9d29e67e
Daniela Astorga Bilbao <25daniela99ab@gmail.com>
2024-05-29 16:56:05 +0200

    Specimen List, Specimen Card and Specimen Details created and updated

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -13,40 +13,39 @@ const Navigation = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="/welcome">WaveScope</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link to="/beaches" className="link">
-              <Nav.Link as="span">Beaches</Nav.Link>
-            </Link>
-            <Link to="/marine-life" className="link">
-              <Nav.Link as="span">Marine Life</Nav.Link>
-            </Link>
-            <Link to="/sightings" className="link">
-              <Nav.Link as="span">Sightings</Nav.Link>
-            </Link>
-            {
-              loggedUser
-              &&
-              <>
+        {
+          loggedUser
+          &&
+          <>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/beaches" className="link">
+                  <Nav.Link as="span">Beaches</Nav.Link>
+                </Link>
+                <Link to="/marine-life" className="link">
+                  <Nav.Link as="span">Marine Life</Nav.Link>
+                </Link>
+                <Link to="/sightings" className="link">
+                  <Nav.Link as="span">Sightings</Nav.Link>
+                </Link>
                 <Nav.Link onClick={logout}>Logout</Nav.Link>
                 <Link to={'/profile'} className="nav-link">Profile</Link>
-
-              </>
-            }
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </>
+        }
       </Container>
     </Navbar>
   )
--- src/components/SpecimenCard/SpecimenCard.jsx
+++ src/components/SpecimenCard/SpecimenCard.jsx
@@ -1,14 +1,14 @@
 import { Card, Container } from "react-bootstrap"
 
-const SpecimenCard = ({ commonName }) => {
+const SpecimenCard = ({ images, commonName }) => {
 
 
   return (
     <Container className="SpecimenCard mt-3">
-
-      <Card style={{ width: '18rem' }}>
+      <Card >
+        <Card.Img variant="top" src={images[0]} />
         <Card.Body>
-          <Card.Title>{commonName}</Card.Title>
+          <Card.Title className="text-center">{commonName}</Card.Title>
         </Card.Body>
       </Card>
 
