e0215496c65b5c03c78d2ce7e490d36cfe9367cf
Daniela Astorga Bilbao <25daniela99ab@gmail.com>
2024-05-30 16:53:18 +0200

    Commit conflicts

--- src/data/lists.data.js
+++ src/data/lists.data.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 export const SPECIMEN_ISENDEMIC = ['Yes', 'No', 'Not sure']
 
 export const SPECIMEN_HABITAT = ['Air', 'Earth', 'Water']
-=======
 export const BEACH_COMPOSITION = ['Sand', 'Rock', 'Gravel', 'Shell', 'Sand & Rock', 'Sand & Gravel', 'Sand & Shell', 'Rock & Gravel', 'Rock & Shell', 'Gravel & Shell']
 
->>>>>>> 022169dbb333b78cb8eb5bb4d28c675c4e6641de
