07da865d5481b004e9e34e11b981df9e7e034519
Paloma Rehecho <paloma_re@hotmail.com>
2024-05-30 18:30:22 +0100

    modal delete done

--- src/pages/BeachDetailsPage/BeachDetailsPage.jsx
+++ src/pages/BeachDetailsPage/BeachDetailsPage.jsx
@@ -1,13 +1,88 @@
-import BeachDetailsCard from '../../components/BeachDetailsCard/BeachDetailsCard'
+import { Button, Card, Carousel, CardBody } from 'react-bootstrap'
 import './BeachDetailsPage.css'
+import { useState, useEffect } from 'react'
+import ModalDelete from '../../components/ModalDelete/ModalDelete'
+import { useParams, Link } from 'react-router-dom'
+import Loader from "../../components/Loader/Loader"
+import beachServices from "../../services/beach.services"
+
 
 const BeachDetailsPage = () => {
 
 
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+
+  const showConfirmModal = () => setShow(true)
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [beach, setBeach] = useState({})
+  const { beachId } = useParams()
+
+  useEffect(() => {
+    loadBeachDetails()
+  }, [])
+
+  const loadBeachDetails = () => {
+
+    beachServices
+      .getOneBeach(beachId)
+      .then(({ data }) => {
+        setBeach(data)
+        setIsLoading(false)
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
+    <div>
+      {
+        isLoading
+          ?
+          <Loader />
+          :
+          <>
+            <Card className="BeachDetailsCard">
+              <Card.Body>
 
-    <BeachDetailsCard />
+                <Carousel>
+                  {beach.images.map((image, index) => (
+                    <Carousel.Item key={index}>
+                      <img className="d-block w-100 img-fluid" style={{ height: 450, objectFit: "cover", objectPosition: "center bottom" }}
+                        src={image}
+                        alt={beach.name} />
+                    </Carousel.Item>
+                  )
+                  )}
+                </Carousel>
+              </Card.Body>
+              <Card.Body>
+                <Card.Title>{`${beach.name}`}</Card.Title>
+                <Card.Text>
+                  {`Location: ${beach.location.coordinates[0]}, ${beach.location.coordinates[1]} `}
+                </Card.Text>
+              </Card.Body>
+              <CardBody>
+                <Card.Text className="Description">
+                  {beach.description}
+                </Card.Text>
+              </CardBody>
 
+              <Card.Body>
+                <Link to='/beaches'>
+                  <Button className="custom-color-button">Nearest beaches</Button>
+                </Link>
+                <Link to='/'>
+                  <Button className="custom-color-button">The fauna today</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+            <Button className='delete-color-button mb-3' onClick={showConfirmModal}> Delete this beach</Button>
+            <ModalDelete show={show} handleClose={handleClose} />
+          </>
+      }
+    </div>
   )
 }
 
